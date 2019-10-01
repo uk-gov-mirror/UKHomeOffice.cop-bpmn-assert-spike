4a9eece117ec25b48aae9052b046386a1382d726
Leigh Anderson <leigh.anderson@digital.homeoffice.gov.uk>
2019-10-01 13:18:06 +0100

    Work through process

--- src/test/java/uk/gov/homeoffice/borders/bpmspike/BpmnProcessTest.java
+++ src/test/java/uk/gov/homeoffice/borders/bpmspike/BpmnProcessTest.java
@@ -8,18 +8,30 @@ import org.junit.Test;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
 
 
+@Deployment(resources = {"test-process.bpmn"})
 public class BpmnProcessTest {
     @Rule
     public ProcessEngineRule processEngineRule = new ProcessEngineRule();
 
     @Test
-    @Deployment(resources = {"test-process.bpmn"})
     public void shouldStartProcess() {
         final var runtimeService = processEngineRule.getRuntimeService();
 
         final var processInstance = runtimeService.startProcessInstanceByKey("TestProcess");
 
-        assertThat(processInstance).isStarted();
+        assertThat(processInstance).isStarted()
+                                   .isWaitingAt("FrobWidget");
+    }
 
+    @Test
+    public void shouldCompleteUserTask() {
+        final var runtimeService = processEngineRule.getRuntimeService();
+        final var taskService = processEngineRule.getTaskService();
+        final var processInstance = runtimeService.startProcessInstanceByKey("TestProcess");
+
+        final var task = taskService.createTaskQuery().taskName("Frob Widget").singleResult();
+        taskService.complete(task.getId());
+
+        assertThat(processInstance).isEnded();
     }
 }
